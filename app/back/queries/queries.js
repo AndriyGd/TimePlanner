db0a7f26132cd256c881d235e2c4040fe0e6055b
Гладкий Андрій Вікторович <a.gladkyi@officeukraine.fozzy.lan>
2017-07-04 11:35:13 +0300

    add REST method

--- app/back/queries/queries.js
+++ app/back/queries/queries.js
@@ -1,41 +1,85 @@
-var promise = require('bluebird');
+let promise = require('bluebird');
 
-var options = {
+let options = {
     promiseLib: promise
 };
 
-var pgp = require('pg-promise')(options);
-var connectionString = "postgres://postgres:ksp.m21d77@localhost:5432/planner";
-var db = pgp(connectionString);
+let pgp = require('pg-promise')(options);
+let connectionString = "postgres://postgres:ksp.m21d77@localhost:5432/planner";
+let db = pgp(connectionString);
 
 function getAllTasks(req, res, next) {
     db.any('select * from tasks')
-        .then(function(data) {
+        .then(data => {
             res.status(200)
                 .json({
                     status: 'success',
                     data: data,
                     message: 'Retrieved All tasks'
                 });
-        }).catch(function(err) {
+        }).catch(err => {
             return next(err);
         });
 }
 
-function getSingleTask() {
-
+function getSingleTask(req, res, next) {
+    let taskId = parseInt(req.params.id);
+    db.one('select * from tasks where id = $1', taskId)
+        .then(data => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrived ONE task'
+                });
+        })
+        .catch(err => {
+            return next(err);
+        });
 }
 
-function createTask() {
-
+function createTask(req, res, next) {
+    db.none('insert into tasks (name, breed, age, sex)' +
+            'values(${name}, ${breed}, ${age}, ${sex})', req.body)
+        .then(() => {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one task'
+                });
+        })
+        .catch(err => {
+            return next(err);
+        });
 }
 
-function updateTask() {
-
+function updateTask(req, res, next) {
+    db.none('update tasks set name = $1, breed = $2, age = $3, sex = $4 where id = $5', [req.body.name, req.body.breed, req.body.age, req.body.sex, parseInt(req.params.id)])
+        .then(() => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: "Update task"
+                });
+        })
+        .catch(err => {
+            return next(err);
+        });
 }
 
-function removeTask() {
-
+function removeTask(req, res, next) {
+    let taskId = parseInt(req.params.id);
+    db.result('delete from tasks where id = $1', taskId)
+        .then(result => {
+            res.status(200)
+                .json({
+                    status: "success",
+                    message: `Removed ${result.rowCount} tasks`
+                });
+        })
+        .catch(err => {
+            return next(err);
+        });
 }
 
 
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var db = require('./app/back/queries/queries');
 
 var router = express.Router();
-var db = require('./app/back/queries/queries');
 
 app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + "/app"));
 
 app.get('/api/tasks', db.getAllTasks);
 app.get('/api/tasks/:id', db.getSingleTask);
 app.post('/api/tasks', db.createTask);
 app.put('/api/tasks/:id', db.updateTask);
 app.delete('/api/tasks/:id', db.removeTask);
-
-
-app.use(express.static(__dirname + "/app"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
