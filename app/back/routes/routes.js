085e72b2494dda6add9f7a0335d9146bed567a35
Гладкий Андрій Вікторович <a.gladkyi@officeukraine.fozzy.lan>
2017-07-04 12:39:33 +0300

    Express - add error handlers

--- app/back/routes/routes.js
+++ app/back/routes/routes.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-var db = require('./app/back/queries/queries');
+var db = require('../queries/queries');
 
 router.get('/api/tasks', db.getAllTasks);
 router.get('/api/tasks/:id', db.getSingleTask);
 router.post('/api/tasks', db.createTask);
 router.put('/api/tasks/:id', db.updateTask);
 router.delete('/api/tasks/:id', db.removeTask);
+
+router.get('/', (req, res) => {
+
+    res.render('index', { title: 'Time Planner' });
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var db = require('./app/back/queries/queries');
+'use-strict'
 
-var router = express.Router();
+let bodyParser = require('body-parser');
+let express = require("express");
+let logger = require('morgan');
+let routes = require('./app/back/routes/routes');
 
 app = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/app"));
+app.use('/', routes);
 
-app.get('/api/tasks', db.getAllTasks);
-app.get('/api/tasks/:id', db.getSingleTask);
-app.post('/api/tasks', db.createTask);
-app.put('/api/tasks/:id', db.updateTask);
-app.delete('/api/tasks/:id', db.removeTask);
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not found');
+    err.status = 404;
+    next(err);
+})
+
+//development error hadler
+//will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.code || 500)
+            .json({
+                status: 'error',
+                message: err
+            });
+    });
+}
+
+//production error handler
+//no stacktraces leked to user
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+        .json({
+            status: 'error',
+            message: err
+        });
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
+
+module.exports = app;
